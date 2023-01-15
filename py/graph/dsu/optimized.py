38f2875c2244658a4a8ab2e3bb3c0727f49ad5ca
Aleksandr Ilin <alexander.ilin.92@gmail.com>
2023-01-15 23:07:32 +0100

    fix rank dsu

--- py/graph/dsu/optimized.py
+++ py/graph/dsu/optimized.py
@@ -2,7 +2,7 @@
 # TC: init - O(V), find/union/connectivity O(Accerman(V))
 class DSU:
     def __init__(self, n):
-        self.parent = list(range(n))
+        self.root = list(range(n))
         self.h = [1] * n
 
     def union(self, x, y):
@@ -12,20 +12,20 @@ class DSU:
         if root_x == root_y:
             return
 
-        if self.h[root_x] == self.h[root_y]:
-            self.parent[root_y] = root_x
-            self.h[root_x] += 1
+        if self.h[root_x] > self.h[root_y]:
+            self.root[root_y] = root_x
         elif self.h[root_x] < self.h[root_y]:
-            self.parent[root_y] = root_x
+            self.root[root_x] = root_y
         else:
-            self.parent[root_x] = root_y
+            self.root[root_y] = root_x
+            self.h[root_x] += 1
 
     def find(self, x):
-        if self.parent[x] == x:
+        if self.root[x] == x:
             return x
 
-        self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
 
     def is_connected(self, x, y):
         return self.find(x) == self.find(y)
